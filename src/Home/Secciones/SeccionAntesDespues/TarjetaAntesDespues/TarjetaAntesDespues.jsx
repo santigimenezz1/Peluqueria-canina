8a5e299b0f167ac5b9b1d9cf843bde66a74b126a
santiago <santiago.manuel.gimenez97@gmail.com>
2024-05-23 00:49:50 +0200

    home v1

--- src/Home/Secciones/SeccionAntesDespues/TarjetaAntesDespues/TarjetaAntesDespues.jsx
+++ src/Home/Secciones/SeccionAntesDespues/TarjetaAntesDespues/TarjetaAntesDespues.jsx
@@ -1,8 +1,18 @@
+import { useState } from 'react';
 import '../TarjetaAntesDespues/tarjetaAntesDespues.css';
 import ReactBeforeSliderComponent from 'react-before-after-slider-component';
 import 'react-before-after-slider-component/dist/build.css';
 
 const TarjetaAntesDespues = ( {imagen1, imagen2} ) => {
+    const [estado, setEstado] = useState(false)
+
+    const cambiarEstado = () => {
+        setEstado(true)
+    }
+    const cambiarEstadoOff = () => {
+        setEstado(false)
+    }
+
     const FIRST_IMAGE = {
         imageUrl: imagen1,
     };
@@ -11,13 +21,13 @@ const TarjetaAntesDespues = ( {imagen1, imagen2} ) => {
     };
 
     return (
-        <div className="seccionAntesDespues">
+        <div onMouseOver={()=>cambiarEstado()} onMouseOut={()=>cambiarEstadoOff()} className="seccionAntesDespues">
             <ReactBeforeSliderComponent
                 firstImage={FIRST_IMAGE}
                 secondImage={SECOND_IMAGE}
             />
-            <h1 className='text__after'>Antes</h1>
-            <h1 className='text__before'>Despues</h1>
+            <h1 className={ estado ? 'text__after' : 'text__afterOn'}>Antes</h1>
+            <h1 className={ estado ? 'text__before' : 'text__beforeOn'}>Despues</h1>
         </div>
     );
 }
