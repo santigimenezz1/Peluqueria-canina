14c980bc66913da1ab46ef2cd78e1b9cc90e01c2
santiago <santiago.manuel.gimenez97@gmail.com>
2024-05-28 23:30:44 +0200

    estilos mapa corregidos

--- src/Contacto/Mapa/Mapa.jsx
+++ src/Contacto/Mapa/Mapa.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../Mapa/mapa.css'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
@@ -11,7 +12,7 @@ const MapComponent = () => {
     const position = [39.453002, -0.354567]; // Coordenadas de la Ciudad de las Artes y las Ciencias
 
   return (
-    <MapContainer center={position} zoom={15} style={{ height: '60vh', width: '65%', borderRadius:"10px" }}>
+    <MapContainer center={position} zoom={15} style={{ height: '60vh', width: '65%', borderRadius:"10px", border:"10px solid rgb(80, 182, 185)", padding:"2px" }}>
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
